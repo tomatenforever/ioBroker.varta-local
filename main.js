af9be911df23b0c70eeb90e4e1e898a97967985c
tomatenforever <mathis.plewa@me.com>
2025-03-04 21:30:03 +0100

    Implement basic version of webrequest

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// filepath: /c:/Users/mathi/source/repos/ioBroker.varta-local/main.js
 "use strict";
 
 /*
@@ -7,176 +8,214 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const axios = require("axios");
+const xml2js = require("xml2js");
 
 class VartaLocal extends utils.Adapter {
 
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "varta-local",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "varta-local",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        // this.on("objectChange", this.onObjectChange.bind(this));
+        // this.on("message", this.onMessage.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Initialize your adapter here
 
-		// Reset the connection indicator during startup
-		this.setState("info.connection", false, true);
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config hostname: " + this.config.hostname);
-		this.log.info("config interval: " + this.config.interval);
+        // The adapters config (in the instance object everything under the attribute "native") is accessible via
+        // this.config:
+        this.log.info("config hostname: " + this.config.hostname);
+        this.log.info("config interval: " + this.config.interval);
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: false,
-			},
-			native: {},
-		});
+        /*
+        For every state in the system there has to be also an object of type state
+        Here a simple template for a boolean variable named "testVariable"
+        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+        */
+        // await this.setObjectNotExistsAsync("testVariable", {
+        //     type: "state",
+        //     common: {
+        //         name: "testVariable",
+        //         type: "boolean",
+        //         role: "indicator",
+        //         read: true,
+        //         write: false,
+        //     },
+        //     native: {},
+        // });
 
-		const interval = this.config.interval * 1000; // Convert to milliseconds
-		this.log.info(`Setting up timer with interval: ${interval} ms`);
+        const interval = this.config.interval * 1000; // Convert to milliseconds
+        this.log.info(`Setting up timer with interval: ${interval} ms`);
 
-		this.timer = setInterval(() => {
-			this.log.info("Timer elapsed, performing scheduled task...");
-			// Add your code here to perform the task you want to run on each interval
-		}, interval);
+        this.timer = setInterval(async () => {
+            this.log.info("Timer elapsed, performing scheduled task...");
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		// this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+            try {
+                const response = await axios.get(`http://${this.config.hostname}/cgi/ems_data.xml`);
+                const xml = response.data;
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		// await this.setStateAsync("testVariable", true);
+                xml2js.parseString(xml, { explicitArray: false }, (err, result) => {
+                    if (err) {
+                        this.log.error("Error parsing XML: " + err);
+                        return;
+                    }
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		// await this.setStateAsync("testVariable", { val: true, ack: true });
+                    const inverter = result.root.inverter;
+                    const vars = inverter.var.reduce((acc, curr) => {
+                        acc[curr.$.name] = curr.$.value;
+                        return acc;
+                    }, {});
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		// await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+                    this.log.info("Parsed values: " + JSON.stringify(vars));
 
-		// examples for the checkPassword/checkGroup functions
-		// let result = await this.checkPasswordAsync("admin", "iobroker");
-		// this.log.info("check user admin pw iobroker: " + result);
+                    // Create objects in ioBroker
+                    for (const [key, value] of Object.entries(vars)) {
+                        this.setObjectNotExistsAsync(`inverter.${key}`, {
+                            type: "state",
+                            common: {
+                                name: key,
+                                type: "number",
+                                role: "value",
+                                read: true,
+                                write: false,
+                            },
+                            native: {},
+                        });
 
-		// result = await this.checkGroupAsync("admin", "admin");
-		// this.log.info("check group user admin group admin: " + result);
-	}
+                        this.setStateAsync(`inverter.${key}`, { val: parseFloat(value), ack: true });
+                    }
+                });
+            } catch (error) {
+                this.log.error("Error making web request: " + error);
+            }
 
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			if (this.timer) {
-				clearInterval(this.timer);
-				this.log.info("Cleared timer on unload");
-			}
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
+        }, interval);
 
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
+        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+        // this.subscribeStates("testVariable");
+        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+        // this.subscribeStates("lights.*");
+        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
+        // this.subscribeStates("*");
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
+        /*
+            setState examples
+            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+        */
+        // the variable testVariable is set to true as command (ack=false)
+        // await this.setStateAsync("testVariable", true);
 
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
+        // same thing, but the value is flagged "ack"
+        // ack should be always set to true if the value is received from or acknowledged from the target system
+        // await this.setStateAsync("testVariable", { val: true, ack: true });
 
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
+        // same thing, but the state is deleted after 30s (getState will return null afterwards)
+        // await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
+        // examples for the checkPassword/checkGroup functions
+        // let result = await this.checkPasswordAsync("admin", "iobroker");
+        // this.log.info("check user admin pw iobroker: " + result);
+
+        // result = await this.checkGroupAsync("admin", "admin");
+        // this.log.info("check group user admin group admin: " + result);
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            if (this.timer) {
+                clearInterval(this.timer);
+                this.log.info("Cleared timer on unload");
+            }
+            // Here you must clear all timeouts or intervals that may still be active
+            // clearTimeout(timeout1);
+            // clearTimeout(timeout2);
+            // ...
+            // clearInterval(interval1);
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
+    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
+    // /**
+    //  * Is called if a subscribed object changes
+    //  * @param {string} id
+    //  * @param {ioBroker.Object | null | undefined} obj
+    //  */
+    // onObjectChange(id, obj) {
+    //  if (obj) {
+    //      // The object was changed
+    //      this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    //  } else {
+    //      // The object was deleted
+    //      this.log.info(`object ${id} deleted`);
+    //  }
+    // }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
+    //  * @param {ioBroker.Message} obj
+    //  */
+    // onMessage(obj) {
+    //  if (typeof obj === "object" && obj.message) {
+    //      if (obj.command === "send") {
+    //          // e.g. send email or pushover or whatever
+    //          this.log.info("send command");
+
+    //          // Send response in callback if required
+    //          if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+    //      }
+    //  }
+    // }
 
 }
 
 if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new VartaLocal(options);
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new VartaLocal(options);
 } else {
-	// otherwise start the instance directly
-	new VartaLocal();
+    // otherwise start the instance directly
+    new VartaLocal();
 }
